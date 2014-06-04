634043df47bec61d1a925e9b763925bf56d5d7ef
Gerardo de La Riva <gerardodiego@gmail.com>
2014-06-04 14:22:05 +0200

    tc.js: Dinamically load plots via Ajax. TODO: Handle async load inside a loop. For now, the code repeats itself for as many times as available plots. (file a bug on this). plot.html: Containers for plots are now loaded dynamically via Ajax. views.py: moved plot functionality out of function home, to a newly created get_plot function. Calculate is called form home() once, and the results stored in session variables. In this way we can update the plots by calling get_plot(). I didn't implement the different options yet, but all is good to go now.

--- web/static/web/js/tc.js
+++ web/static/web/js/tc.js
@@ -1,6 +1,69 @@
 /* Script to manage tc web app */
+$( ).ready(function(){	
 
-$( ).ready(function(){
+
+//Load the default plots via ajax:
+	
+
+var availablePlots = [ 'xyz', 'xy', 'lms', 'lms_base', 'bm', 'lm' ];
+
+
+//@TODO: Need to asynch this. I will repeat the code for now:
+
+//xyz
+			$.get( "/get_plot/" + availablePlots[0])
+				.done(function( data ) {
+					$( "div#" + availablePlots[0] +"_plot" ).append(data);
+  				})
+  				.fail(function() {
+    				alert( "error when getting " + availablePlots[0] + " plot from server" );
+				});	
+
+//xyz
+			$.get( "/get_plot/" + availablePlots[1])
+				.done(function( data ) {
+					$( "div#" + availablePlots[1] +"_plot" ).append(data);
+  				})
+  				.fail(function() {
+    				alert( "error when getting " + availablePlots[1] + " plot from server" );
+				});	
+
+//lms
+			$.get( "/get_plot/" + availablePlots[2])
+				.done(function( data ) {
+					$( "div#" + availablePlots[2] +"_plot" ).append(data);
+  				})
+  				.fail(function() {
+    				alert( "error when getting " + availablePlots[2] + " plot from server" );
+				});	
+
+// lms_base
+			$.get( "/get_plot/" + availablePlots[3])
+				.done(function( data ) {
+					$( "div#" + availablePlots[3] +"_plot" ).append(data);
+  				})
+  				.fail(function() {
+    				alert( "error when getting " + availablePlots[3] + " plot from server" );
+				});	
+
+//bm
+			$.get( "/get_plot/" + availablePlots[4])
+				.done(function( data ) {
+					$( "div#" + availablePlots[4] +"_plot" ).append(data);
+  				})
+  				.fail(function() {
+    				alert( "error when getting " + availablePlots[4] + " plot from server" );
+				});	
+
+//lm
+
+			$.get( "/get_plot/" + availablePlots[5])
+				.done(function( data ) {
+					$( "div#" + availablePlots[5] +"_plot" ).append(data);
+  				})
+  				.fail(function() {
+    				alert( "error when getting " + availablePlots[5] + " plot from server" );
+				});	
 
 //Initialization of jQuery UI tabs
 
@@ -12,6 +75,7 @@ $( ).ready(function(){
 
 //@TODO: Need to make navigation buttons (home, magnifier, cross) more visible.
 	$( "image" ).css("opacity", 1); //To make the nav elements more visible.
+	
 
 //Start by showing XYZ plot + data:
 
--- web/views.py
+++ web/views.py
@@ -17,51 +17,44 @@ import mpld3
 from django.utils.safestring import mark_safe
 from celery import shared_task
 from numpy import *
-	
-def gimmeAjax(request):
 
-	return HttpResponse("Some data from the server sent via Ajax!");
+def get_plot(request, plot):
+	
+	fig = plt.figure()
+	ax = fig.add_subplot(111)
+	plots = request.session['plots']
+	results = request.session['results']
+	
+	options = { 'grid' : True,
+            	'cie31' : True,
+            	'cie64' : False,
+            	'labels' : True,
+            	'label_fontsize' : 12 }
+            	
+	if (plot == 'xyz'):
+		tc182.plot.xyz(ax, plots, options)
+		
+	elif (plot == 'xy'):
+		tc182.plot.xy(ax, plots, options)
+	
+	elif (plot == 'lms'):
+		tc182.plot.lms(ax, plots, options)
+	
+	elif (plot == 'lms_base'):
+		tc182.plot.lms_base(ax, plots, options)
+	
+	elif (plot == 'bm'):
+		tc182.plot.bm(ax, plots, options)
+	
+	elif (plot == 'lm'):
+		tc182.plot.lm(ax, plots, options)
+		
+	theFig = mark_safe(mpld3.fig_to_html(fig, template_type='general'))
+	resulting_plot = theFig;
+	return HttpResponse(resulting_plot);
+
 
 def home(request):
-
-#	Returns
-#    -------
-#    xyz : ndarray
-#       	The computed colour matching functions.
-#    cc : ndarray
-#        The chromaticity coordinates.
-#    cc_white : ndarray
-#        The chromaticity coordinates for equi-energy stimulus.
-#    mat : ndarray
-#        The 3x3 matrix for converting from LMS to XYZ.
-#    lms_standard : ndarray
-#        The computed LMS functions at the given standard resolution.
-#    lms_base : ndarray
-#        The computed LMS functions at full available resolution (9 sign. fig.).        
-#    bm : ndarray
-#        The computed Boynton-MacLeod chromaticity coordinates.
-#    bm_white : ndarray
-#        The Boynton-MacLeod coordinates for equi-energy stimulus.
-#    lm : ndarray
-#        The computed normalised lm coordinates.
-#    lm_white : ndarray
-#        The lm coordinates for equi-energy stimulus.
-#    lambda_test_min : int
-#        The wavelength of minimum x chromaticity value.
-#    purple_line_cc : ndarray
-#        Chromaticity coordinates for the endpoints of the purple line.
-#    purple_line_bm : ndarray
-#        Boynton-MacLeod coordinates for the endpoints of the purple line.
-#    purple_line_lm : ndarray
-#        lm coordinates for the endpoints of the purple line.
-#    plots : dict
-#        Versions of xyz, cc, lms, bm, lm at 0.1 nm for plotting. Includes also CIE1964 and CIE1931 data. 
-
-	#field_size = 2.0;
-	#age = 32;
-	#lambda_min = 390.0;
-	#lambda_max = 830.0;
-	#lambda_step = 1.0;
 
 	try:
 		field_size = float(request.POST["field_size"])
@@ -99,54 +92,34 @@ def home(request):
 		print "lambda_step: %s" % lambda_step
 		
 	html_list = [] #List containing all the theDescriptions
-	fig_list = []  #List containing all the figures/plots
 	tab_list = []  #List containing all the tabulated data
 	
-	fig = plt.figure()
-	ax = fig.add_subplot(111)
+	results, plots = tc182.compute_tabulated(field_size, age, lambda_min, lambda_max, lambda_step)
 	
-	results, plots = tc182.compute_tabulated(field_size, age, lambda_min, lambda_max, lambda_step)	
-	options = { 'grid' : True,
-            	'cie31' : True,
-            	'cie64' : False,
-            	'labels' : True,
-            	'label_fontsize' : 12 }
+	request.session['results'] = results
+	request.session['plots'] = plots
+	
     #0 #xyz 
-	
-	tc182.plot.xyz(ax, plots, options)
 	
 	theDescription = mark_safe(tc182.description.xyz(results,'XYZ'))
 	html_list.append(theDescription)
-	
-	theFig = mark_safe(mpld3.fig_to_html(fig, template_type='general'))
-	fig_list.append(theFig)
 	
 	theTable = mark_safe(tc182.table.xyz(results));
 	tab_list.append(theTable)
 	
     #1 #xy
-    
-	tc182.plot.xy(ax, plots, options)
 	
 	theDescription = mark_safe(tc182.description.xy(results,'XY'))
 	html_list.append(theDescription)
 	
-	theFig = mark_safe(mpld3.fig_to_html(fig, template_type='general'))
-	fig_list.append(theFig)
-
 	theTable = mark_safe(tc182.table.xy(results));
 	tab_list.append(theTable)
 
 	
 	#2 #lms
-    
-	tc182.plot.lms(ax, plots, options)
 	
 	theDescription = mark_safe(tc182.description.lms(results,'LMS'))
 	html_list.append(theDescription)
-	
-	theFig = mark_safe(mpld3.fig_to_html(fig, template_type='general'))
-	fig_list.append(theFig)
 	
 	theTable = mark_safe(tc182.table.lms(results));
 	tab_list.append(theTable)
@@ -154,99 +127,32 @@ def home(request):
 	
 	#3 #lms_base
     
-	tc182.plot.lms_base(ax, plots, options)
-	
 	theDescription = mark_safe(tc182.description.lms_base(results,'LMS BASE'))
 	html_list.append(theDescription)
-	
-	theFig = mark_safe(mpld3.fig_to_html(fig, template_type='general'))
-	fig_list.append(theFig)
 	
 	theTable = mark_safe(tc182.table.lms_base(results));
 	tab_list.append(theTable)
 
 	#4 #bm
 	
-	tc182.plot.bm(ax, plots, options)
-	
 	theDescription = mark_safe(tc182.description.bm(results,'BM'))
 	html_list.append(theDescription)
-	
-	theFig = mark_safe(mpld3.fig_to_html(fig, template_type='general'))
-	fig_list.append(theFig)
 	
 	theTable = mark_safe(tc182.table.bm(results));
 	tab_list.append(theTable)
 
 	#5 #lm
 	
-	tc182.plot.lm(ax, plots, options)
-	
 	theDescription = mark_safe(tc182.description.lm(results,'LM'))
 	html_list.append(theDescription)
-	
-	theFig = mark_safe(mpld3.fig_to_html(fig, template_type='general'))
-	fig_list.append(theFig)
 	
 	theTable = mark_safe(tc182.table.lm(results));
 	tab_list.append(theTable)
 	
-	# #6 #xyz31
-# 	
-# 	tc182.plot.xyz31(ax, plots, options)
-# 	
-# 	theDescription = mark_safe(tc182.description.xyz31(results,'XYZ31'))
-# 	html_list.append(theDescription)
-# 	
-# 	theFig = mark_safe(mpld3.fig_to_html(fig, template_type='general'))
-# 	fig_list.append(theFig)
-# 	
-# 	theTable = mark_safe(tc182.table.xyz31(results));
-# 	tab_list.append(theTable)
-# 	
-# 	#7 #xyz64
-# 
-# 	tc182.plot.xyz64(ax, plots, options)
-# 	
-# 	theDescription = mark_safe(tc182.description.xyz64(results,'XYZ64'))
-# 	html_list.append(theDescription)
-# 	
-# 	theFig = mark_safe(mpld3.fig_to_html(fig, template_type='general'))
-# 	fig_list.append(theFig)
-# 	
-# 	theTable = mark_safe(tc182.table.xyz64(results));
-# 	tab_list.append(theTable)
-# 
-# 	
-# 	#8 #xy31
-# 	
-# 	tc182.plot.xy31(ax, plots, options)
-# 	
-# 	theDescription = mark_safe(tc182.description.xy31(results,'XY31'))
-# 	html_list.append(theDescription)
-# 	
-# 	theFig = mark_safe(mpld3.fig_to_html(fig, template_type='general'))
-# 	fig_list.append(theFig)
-# 	
-# 	theTable = mark_safe(tc182.table.xy31(results));
-# 	tab_list.append(theTable)
-# 
-# 	
-# 	#9 #xy64
-# 	tc182.plot.xy64(ax, plots, options)
-# 	
-# 	theDescription = mark_safe(tc182.description.xy64(results,'XY64'))
-# 	html_list.append(theDescription)
-# 	
-# 	theFig = mark_safe(mpld3.fig_to_html(fig, template_type='general'))
-# 	fig_list.append(theFig)
-# 	
-# 	theTable = mark_safe(tc182.table.xy64(results));
-# 	tab_list.append(theTable)
+
 
 
 	context = { 'tab_list' : tab_list,
-				'fig_list' : fig_list,
 				'html_list' : html_list,
 				'field_size' : field_size,
 				'age'	: age,
