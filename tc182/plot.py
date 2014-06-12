d3e3be3b19304cdf05c8dd7b14112eac7ef35d73
Ivar Farup <ivar.farup@hig.no>
2014-06-12 17:37:18 +0200

    #71 fixed

--- tc182/plot.py
+++ tc182/plot.py
@@ -356,7 +356,7 @@ def lm(axes, plots, options):
                                                                            plots['lambda_step']),
                                  fontsize=16)
     if options['full_title']:
-        axes.set_title('Maxwellian $lm$ chromaticity diagram\nField size: ' + str(plots['field_size']) +
+        axes.set_title('Maxwellian lm chromaticity diagram\nField size: ' + str(plots['field_size']) +
                             u'\N{DEGREE SIGN},  Age: ' + str(plots['age']) +
                             u' yr,  Domain: %s\u2013%s nm' % (plots['lambda_min'],
                                                               plots['lambda_max']) +
