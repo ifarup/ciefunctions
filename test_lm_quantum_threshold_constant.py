45684da34047c1ce780a94e18535d9f11b87af11
Ivar Farup <ivar.farup@hig.no>
2014-12-02 20:35:36 +0100

    testing two alternative methods for constant

--- test_lm_quantum_threshold_constant.py
+++ test_lm_quantum_threshold_constant.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-testconstant: CIE TC1-82 Computations
+test_lm_quantum_threshold_constant: CIE TC1-82 Computations
 
 Copyright (C) 2014 Ivar Farup and Jan Henrik Wold
 
@@ -32,7 +32,22 @@ for fs in fs_range:
         lmsq = tc182.lms_quantal(fs, age)
         const = abt[:,1] * lmsq[:,2] / (abt[:,2] * lmsq[:,1])
         consts.append(const[0])
-
 consts = np.array(consts)
 plt.plot(consts, '.')
+
+age = 32
+fs = 2
+abt = tc182.absorpt(fs)
+lmsq = tc182.lms_quantal(fs, age)
+const = abt[:,1] * lmsq[:,2] / (abt[:,2] * lmsq[:,1])
+print(const[0])
+
+ocul = tc182.ocular(age)
+tau = 10**(-tc182.d_mac_max(fs)*tc182.VisualData.macula[:,1]/.35 - ocul[:,1])
+i_max_alpha_L = np.argmax(abt[:,1])
+i_max_alpha_M = np.argmax(abt[:,2])
+
+const2 = lmsq[i_max_alpha_M,2] * tau[i_max_alpha_L] / (lmsq[i_max_alpha_L,1] * tau[i_max_alpha_M])
+print(const2)
+
 plt.show()
