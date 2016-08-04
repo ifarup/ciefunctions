5c8a06a8130b33ca09bbcba38cb04ca5160a7008
Ivar Farup <ivar.farup@hig.no>
2016-08-04 14:11:50 +0200

    slettet tull etc.

--- tc182/plot.py
+++ tc182/plot.py
@@ -19,8 +19,6 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-# Tull og tøys
-
 import numpy as np
 import threading
 
