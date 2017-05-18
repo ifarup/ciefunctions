6c127b21d7477c6c2a74e0430f277132ebbea72c
Ivar Farup <ivar.farup@ntnu.no>
2017-05-18 23:44:55 +0200

    minor

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
 from setuptools import setup
 
 setup(
-    name="CIE Functions",
+    name="ciefunctions",
     version="1.0.0b2",
     packages=['tc1_97', 'web'],
     scripts=['ciefunctions.py'],
     include_package_data=True,
     package_data={'tc1_97': ['data/*'],
-                  'web': ['static/*'] },
+                  'web': ['static/*']},
     install_requires=[
         'numpy',
         'scipy',
         'matplotlib',
-        'pyqt5'
-    ],
+        'pyqt5'],
     author="Jan Henrik Wold and Ivar Farup",
     author_email="ivar.farup@ntnu.no",
     description="Desktop and web apps for computing the CIE Functions",
